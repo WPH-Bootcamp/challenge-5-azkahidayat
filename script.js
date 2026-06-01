2b8846d93e6c10d0813bba4ea74d7682ac495993
azkahidayat <azka.reksalegora12@gmail.com>
2026-06-01 19:54:04 +0700

    feat: add functional delete button

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ class TodoList {
     this.todos.push(newTodo);
   }
 
-  removeTodo(id) {
+  deleteTodo(id) {
     this.todos = this.todos.filter((todo) => todo.id !== id);
   }
 
@@ -89,47 +89,49 @@ function hideError() {
   todoInput.classList.remove('border-red-500');
 }
 
-function updateUI(todo, id) {
+function updateTodoListUI(todo, id) {
   const todoElement = `
   <li class="todo flex justify-between items-center px-1 py-2" data-id="${id}">
   <div class="toggle-todo flex gap-2 items-center w-full cursor-pointer">
     <div
       class="mark-todo size-4 flex justify-center items-center rounded-md border cursor-pointer"
-    >
-      
-    </div>
+    ></div>
     <p class="text-todo md:text-lg capitalize pointer-events-none">${todo}</p>
   </div>
   <div class="flex gap-2">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      class="size-5 cursor-pointer"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-      />
-    </svg>
+    <button class="delete-button">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-5 cursor-pointer"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+        />
+      </svg>
+    </button>
 
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      class="size-5 cursor-pointer"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-      />
-    </svg>
+    <div class="edit-button">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-5 cursor-pointer"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+        />
+      </svg>
+    </div>
   </div>
 </li>
   
@@ -148,7 +150,7 @@ function handleAddTodo() {
   const todo = new Todo(newTodoUserInput);
   todoList.addTodo(todo);
 
-  updateUI(todo.todo, todo.id);
+  updateTodoListUI(todo.todo, todo.id);
 
   todoInput.value = '';
 }
@@ -171,7 +173,31 @@ document.addEventListener('click', function (event) {
     // 4. Update UI only the clicked todo
     updateToggleTodoUI(markTodo, clickedElementTodo, clickedObjectTodo);
   }
+
+  // === DELETE TODO ===
+  const isDeleteButton = event.target.closest('.delete-button');
+  if (isDeleteButton) {
+    // 1. Access id
+    const clickedElementTodo = isDeleteButton.closest('.todo');
+    const idTodo = clickedElementTodo.dataset.id;
+
+    // 2. Delete
+    todoList.deleteTodo(idTodo);
+
+    // 3. rerender
+
+    renderTodos();
+  }
 });
+
+function renderTodos() {
+  console.log(todoContainer);
+  todoContainer.innerHTML = '';
+
+  todoList.todos.forEach((todo) => {
+    updateTodoListUI(todo.todo, todo.id);
+  });
+}
 
 function updateToggleTodoUI(markTodo, clickedElementTodo, clickedObjectTodo) {
   const textTodo = clickedElementTodo.querySelector('.text-todo');
@@ -203,7 +229,6 @@ function updateToggleTodoUI(markTodo, clickedElementTodo, clickedObjectTodo) {
   }
 }
 
-// === DELETE TODO ===
 // === EDIT TODO ===
 
 // document.addEventListener('DOMContentLoaded', async function () {
