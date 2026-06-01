81b588d7b7179c6311ba0380d5007517b7a61874
azkahidayat <azka.reksalegora12@gmail.com>
2026-06-01 16:36:30 +0700

    feat: add Todo and TodoList class wifh fetch function

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
-console.log('test');
+class TodoList {
+  constructor() {
+    this.todos = [];
+  }
+
+  addTodo(newTodo) {
+    this.todos.push(newTodo);
+  }
+
+  removeTodo(id) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+
+  getAll() {
+    return this.todos;
+  }
+}
+
+class Todo {
+  constructor(id, todo, completed) {
+    this.id = id;
+    this.todo = todo;
+    this.completed = completed;
+  }
+
+  toggleTodo() {
+    this.completed = !this.completed;
+  }
+
+  editTodo(newTodo) {
+    this.todo = newTodo;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
+  const data = await fetchData();
+  const todos = data.todos;
+  const todosList = new TodoList();
+
+  todos.forEach((t) => {
+    const todo = new Todo(t.id, t.todo, t.completed);
+    todosList.addTodo(todo);
+  });
+
+  console.log(todosList.getAll());
+});
+
+async function fetchData() {
+  try {
+    const res = await fetch('https://dummyjson.com/todo');
+    if (!res.ok) {
+      throw new Error(`Status: ${res.status}`);
+    }
+    const data = await res.json();
+
+    return data;
+  } catch (err) {
+    return err;
+  }
+}
