e77c1f5b10d79fe648513483152bafb4b6de62c3
azkahidayat <azka.reksalegora12@gmail.com>
2026-06-01 20:35:45 +0700

    feat: add live task tracker for total,completed,and incomplete

--- script.js
+++ script.js
@@ -43,8 +43,8 @@ class TodoList {
 class Todo {
   constructor(todo, completed, id) {
     this.todo = todo;
-    this.completed = completed ? complete : false;
-    this.id = id ? id : Date.now() + Math.random().toString(10).slice(2);
+    this.completed = completed ? completed : false;
+    this.id = id ? id : Date.now() + Math.random();
   }
 
   toggleTodo() {
@@ -58,29 +58,105 @@ class Todo {
 
 const todoList = new TodoList();
 
-// === ADD TODO ===
-const todoInput = document.querySelector('#input-todo');
+// === TASK NUMBER ===
+const taskNumber = document.querySelector('#task-number');
+updateTaskNumber();
+
+function updateTaskNumber() {
+  const tasks = todoList.getAll().length;
+  taskNumber.innerText = tasks;
+}
+
+// === INCOMPLETE TASK NUMBER ===
+const incompleteTaskNumber = document.querySelector('#incomplete-task-number');
+function updateIncompleteTaskNumber() {
+  const incompleteTasks = todoList
+    .getAll()
+    .filter((todo) => !todo.completed).length;
+  incompleteTaskNumber.innerText = incompleteTasks;
+}
+updateIncompleteTaskNumber();
+
+// === COMPLETE TASK NUMBER ===
+const completeTaskNumber = document.querySelector('#complete-task-number');
+function updateCompleteTaskNumber() {
+  const completeTasks = todoList
+    .getAll()
+    .filter((todo) => todo.completed).length;
+
+  completeTaskNumber.innerText = completeTasks;
+}
+updateCompleteTaskNumber();
+
+// === ADD TODO FUNCTIONALITY ===
+const todoInput = document.querySelector('#todo-input');
 const todoAddButton = document.querySelector('#todo-add-button');
 const errorText = document.querySelector('#error-text');
 const todoContainer = document.querySelector('#todo-container');
 
 let newTodoUserInput = '';
 
-todoInput.addEventListener('input', function (event) {
-  newTodoUserInput = todoInput.value.trim();
-});
-
+// 1. Add Todo by pressing Enter button
 todoInput.addEventListener('keydown', function (event) {
+  newTodoUserInput = todoInput.value.trim();
   event.key === 'Enter' && handleAddTodo();
 });
 
+// 2. Add Todo by clicking Add Todo button
 todoAddButton.addEventListener('click', function () {
+  newTodoUserInput = todoInput.value.trim();
   handleAddTodo();
 });
 
+const checkSvg = `
+<svg
+xmlns="http://www.w3.org/2000/svg"
+viewBox="0 0 24 24"
+fill="#3F9CA1"
+class="size-5 "
+>
+<path
+fill-rule="evenodd"
+d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+clip-rule="evenodd"
+/>
+</svg> 
+
+`;
+
+function handleAddTodo() {
+  // 1. validation
+  if (!newTodoUserInput) {
+    showError();
+    return;
+  }
+
+  hideError();
+
+  // 2. Create object todo
+  const todo = new Todo(newTodoUserInput);
+
+  // 3. Add todo
+  todoList.addTodo(todo);
+
+  // 4. Render todo
+  renderTodos();
+
+  // 5. Update task Number
+  updateTaskNumber();
+
+  // 6. Update Incomplete task number
+  updateIncompleteTaskNumber();
+
+  // 7. Save local
+  saveDataToLocalStorage(todo);
+
+  todoInput.value = '';
+  newTodoUserInput = '';
+}
+
 function showError() {
   errorText.classList.remove('invisible');
-  todoInput.classList.remove('border-[#3F9CA1]');
   todoInput.classList.add('border-red-500');
 }
 
@@ -91,12 +167,12 @@ function hideError() {
 
 function updateTodoListUI(todo, id) {
   const todoElement = `
-  <li class="todo flex justify-between items-center px-1 py-2" data-id="${id}">
+  <li class="todo flex justify-between items-center px-1 py-2 hover:bg-[#E8F3F5] ${completed ? 'bg-[#E8F3F5]' : ''}" data-id="${id}">
   <div class="toggle-todo flex gap-2 items-center w-full cursor-pointer">
     <div
-      class="mark-todo size-4 flex justify-center items-center rounded-md border cursor-pointer"
-    ></div>
-    <p class="text-todo md:text-lg capitalize pointer-events-none">${todo}</p>
+      class="mark-todo size-4 flex justify-center items-center rounded-md border cursor-pointer ${completed ? 'border-0' : 'border'}"
+    >${completed ? checkSvg : ''}</div>
+    <p class="text-todo md:text-lg capitalize pointer-events-none ${completed ? 'line-through text-gray-500' : ''}">${todo}</p>
   </div>
   <div class="flex gap-2">
     <button class="delete-button">
